36e7e12963728efe69fa90a1ec0e877642cdb333
Vincent Robert <vincent.robert@cantor.fr>
2014-04-07 16:53:39 +0200

    Change register to use rbenv exec instead of Ruby shim

--- libexec/rbenv_register.cmd
+++ libexec/rbenv_register.cmd
@@ -6,7 +6,7 @@
 @reg add "HKCR\.rb" /ve /t REG_SZ /d "RubyFile" /f > NUL
 @reg add "HKCR\RubyFile" /ve /t REG_SZ /d "Ruby File" /f > NUL
 @reg add "HKCR\RubyFile\DefaultIcon" /ve /t REG_SZ /d "%RBENV_ROOT%\resources\ruby.ico" /f > NUL
-@reg add "HKCR\RubyFile\shell\open\command" /ve /t REG_SZ /d "\"%RBENV_ROOT%\shims\ruby.cmd\" \"%%1\" %%*" /f > NUL
+@reg add "HKCR\RubyFile\shell\open\command" /ve /t REG_SZ /d "\"%RBENV_ROOT%\libexec\rbenv_exec.cmd\" \"%%1\" %%*" /f > NUL
 
 @ echo(Ruby files are now associated with rbenv-cmd.
 
