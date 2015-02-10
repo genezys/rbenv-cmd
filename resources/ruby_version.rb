f77cc8658fac71be8edc7e186c277e15443894cb
Vincent Robert <vincent.robert@cantor.fr>
2015-02-10 10:23:24 +0100

    Add support for semantic versioning introduced in Ruby 2.1

--- resources/ruby_version.rb
+++ resources/ruby_version.rb
@@ -1,6 +1,12 @@
+require "rubygems"
+
 puts case RUBY_PLATFORM
 	when 'java'
 		"jruby-#{JRUBY_VERSION}"
 	else
-		"#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"
+		if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.1')
+			RUBY_VERSION
+		else
+			"#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"
+		end
 end
